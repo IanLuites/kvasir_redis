864227719e7bc6a699aa0e780174541994aac225
Ian Luites <IanLuites@users.noreply.github.com>
2020-01-28 23:11:23 +0100

    Improve key storage in redis cache.

--- lib/kvasir_redis/cache.ex
+++ lib/kvasir_redis/cache.ex
@@ -13,7 +13,7 @@ if Code.ensure_loaded?(Kvasir.Agent) do
                agent,
                [
                  "SET",
-                 "cache.#{id}",
+                 key(agent, id),
                  :erlang.term_to_binary({offset, data}, compressed: 9, minor_version: 2)
                ]
              ),
@@ -22,12 +22,15 @@ if Code.ensure_loaded?(Kvasir.Agent) do
 
     @impl Kvasir.Agent.Cache
     def load(agent, id) do
-      with {:ok, result} when result != nil <- Redis.command(agent, ["GET", "cache.#{id}"]),
+      with {:ok, result} when result != nil <- Redis.command(agent, ["GET", key(agent, id)]),
            {offset, data} <- :erlang.binary_to_term(result) do
         {:ok, offset, data}
       else
         {:ok, nil} -> {:error, :not_found}
       end
     end
+
+    @spec key(module, term) :: String.t()
+    defp key(agent, id), do: "cache:#{inspect(agent)}:#{id}"
   end
 end
